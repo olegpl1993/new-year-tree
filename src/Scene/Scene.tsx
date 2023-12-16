67f1df773fd4b684f2b8c6298cbc16e01459c010
Oleg <olegpl1993@gmail.com>
2023-12-16 22:40:54 +0200

    refactor: EnvironmentObjects

--- src/Scene/Scene.tsx
+++ src/Scene/Scene.tsx
@@ -1,15 +1,9 @@
 import { Canvas } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
-import PointLight from "./PointLight/PointLight";
 import Tree from "./Tree/Tree";
 import ItemModel from "./ItemModel/ItemModel";
-import Box from "./Box/Box";
-import Floor from "./Floor/Floor";
-import Floor2 from "./Floor2/Floor2";
-import Window from "./Window/Window";
 import { Item } from "../types";
-import Door from "./Door/Door";
-import Poster from "./Poster/Poster";
+import EnvironmentObjects from "./EnvironmentObjects/EnvironmentObjects";
 
 interface Props {
   items: Item[];
@@ -21,11 +15,18 @@ function Scene(props: Props) {
 
   return (
     <Canvas shadows>
-      <ambientLight intensity={0.5} />
-      <PointLight position={[2, 4.5, 2]} color="red" intensity={30} point />
-      <PointLight position={[-2, 4.5, 2]} color="blue" intensity={30} point />
-      <PointLight position={[2, 4.5, -2]} color="yellow" intensity={30} point />
-      <PointLight position={[-2, 4.5, -2]} color="green" intensity={30} point />
+      <Tree position={[0, 0, 0]} items={items} changeByIndex={changeByIndex} />
+
+      {items.map((item, index) => (
+        <ItemModel
+          key={index}
+          item={item}
+          index={index}
+          changeByIndex={changeByIndex}
+        />
+      ))}
+
+      <EnvironmentObjects />
 
       <OrbitControls
         target={[0, 1.75, 0]}
@@ -36,28 +37,7 @@ function Scene(props: Props) {
         enablePan={false}
       />
 
-      <Tree position={[0, 0, 0]} items={items} changeByIndex={changeByIndex} />
-      {items.map((item, index) => (
-        <ItemModel
-          key={index}
-          item={item}
-          index={index}
-          changeByIndex={changeByIndex}
-        />
-      ))}
-
-      <Door position={[4.94, 1.75, 0]} rotation={[0, -1.57, 0]} />
-      <Window position={[0, 2, -4.94]} rotation={[0, 0, 0]} />
-      <Window position={[-4.94, 2, 0]} rotation={[0, 1.57, 0]} />
-      <Poster position={[0, 2.5, 4.94]} rotation={[0, 3.14, 0]} />
-
-      <Box position={[0, 2.5, 5]} rotation={[0, 0, 0]} />
-      <Box position={[5, 2.5, 0]} rotation={[0, 1.57, 0]} />
-      <Box position={[-5, 2.5, 0]} rotation={[0, 1.57, 0]} />
-      <Box position={[0, 2.5, -5]} rotation={[0, 0, 0]} />
-
-      <Floor position={[0, 0, 0]} rotation={[-1.57, 0, 0]} />
-      <Floor2 position={[0, 5, 0]} rotation={[1.57, 0, 0]} />
+      <ambientLight intensity={0.5} />
 
       <Environment preset="lobby" background />
     </Canvas>
